7462453819cb036e0822e0134b06f322f7e24e72
Tyler Boutwell <tylerboutwell1@gmail.com>
2025-10-19 10:46:23 -0500

    create core and user apps. create base, home templates and static css file

--- minipage/settings.py
+++ minipage/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
+    'core.apps.CoreConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
