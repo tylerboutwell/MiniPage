c7bc2c2f51ed538f1f6650df5bdc5f1df8c1aa67
Tyler Boutwell <tylerboutwell1@gmail.com>
2025-10-31 17:57:14 -0500

    create a starting login page at /login using Django's LoginView

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 from .views import views, user_views
+from django.contrib.auth import views as auth_views
 
 app_name = "core"
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path('login/', auth_views.LoginView.as_view(), name="login"),
 ]
