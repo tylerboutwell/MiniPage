c6fcf436cad970f5e399d01b441bce88ce2cf450
Tyler Boutwell <tylerboutwell1@gmail.com>
2025-11-06 06:11:37 -0600

    remove login code from home view, it's all included with loginview

--- core/views/user_views.py
+++ core/views/user_views.py
@@ -4,21 +4,6 @@ from django.shortcuts import render, redirect
 from ..forms import SignUpForm
 from profiles.models import Profile
 
-def home(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            messages.success(request, 'You are now logged in')
-            return redirect('core:home')
-        else:
-            messages.success(request, 'Invalid username or password')
-            return redirect('core:home')
-    else:
-        return render(request, 'core/home.html')
-
 def register(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
