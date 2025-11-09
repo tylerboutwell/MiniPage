afb7776a34290ad039b1d2f1aa499e341a68457f
Tyler Boutwell <tylerboutwell1@gmail.com>
2025-11-09 13:15:55 -0600

    clean up code and change error to warning message instead of success

--- profiles/views.py
+++ profiles/views.py
@@ -3,16 +3,16 @@ from profiles.forms import ProfileForm
 from django.contrib import messages
 
 def create_profile(request):
-    form = ProfileForm(request.POST or None)
-    if request.user.is_authenticated:
-        if request.method == "POST":
-            if form.is_valid():
-                create_profile = form.save(commit=False)
-                create_profile.user = request.user
-                create_profile.save()
-                messages.success(request, "You have created your profile!")
-                return redirect('core:home')
-        return render(request, 'profiles/create_profile.html', {'form': form})
-    else:
-        messages.success(request, "You must be logged in to create profile.")
+    if not request.user.is_authenticated:
+        messages.warning(request, "You must be logged in to create profile.")
         return redirect('core:home')
+
+    form = ProfileForm(request.POST or None)
+    if request.method == "POST":
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = request.user
+            profile.save()
+            messages.success(request, "You have created your profile!")
+            return redirect('core:home')
+    return render(request, 'profiles/create_profile.html', {'form': form})
