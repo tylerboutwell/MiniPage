1e0c2b893c3c0625e3f048ffae9bb674fb282b9a
Tyler Boutwell <tylerboutwell1@gmail.com>
2025-11-09 17:28:11 -0600

    Add condition to check if username isn't a reserved username to avoid url conflicts

--- core/views/user_views.py
+++ core/views/user_views.py
@@ -1,16 +1,24 @@
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.db.transaction import commit
 from django.shortcuts import render, redirect
 from ..forms import SignUpForm
 from profiles.models import Profile
 
+RESERVED_USERNAMES = {"login", "register", "admin", "about", "contact", "logout", "profile"}
 def register(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
-            form.save()
-            username = form.cleaned_data['username']
+            form = form.save(commit=False)
+            username = form.cleaned_data['username'].lower()
+            #Check if username isn't a reserved URL
+            if username in RESERVED_USERNAMES:
+                form.add_error("username", "This username is not allowed.")
+                return render(request, "registration/register.html", {"form": form})
+
             password = form.cleaned_data['password1']
+            form.save()
             user = authenticate(username=username, password=password)
             # Create a profile instance when a user is registered
             if user is not None:
