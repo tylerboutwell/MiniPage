6691a55af636630719172bb3d8ca59af4dd21385
Tyler Boutwell <tylerboutwell1@gmail.com>
2025-11-10 18:02:22 -0600

    when retrieving minipage, use username as parameter, use that username to get the user object, then use that to get the related profile

--- core/views/views.py
+++ core/views/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from profiles.models import Profile
+from django.contrib.auth.models import User
 
 def home(request):
     return render(request, 'core/home.html')
 
-def minipage_view(request):
-    profile = get_object_or_404(Profile, user=request.user)
+def minipage_view(request, username):
+    user = get_object_or_404(User, username=username)
+    profile = get_object_or_404(Profile, user=user)
     links = profile.links.all()
     return render(request, 'core/minipage.html', {'links': links, profile: profile})
