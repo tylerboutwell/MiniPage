c56ff7998d2b0cf0d7725ea795fe508adb63bd57
Tyler Boutwell <tylerboutwell1@gmail.com>
2025-11-15 17:16:26 -0600

    style the profile form

--- profiles/forms.py
+++ profiles/forms.py
@@ -7,6 +7,12 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         exclude = ['user']
+        widgets = {
+            'profile_name': forms.TextInput(attrs={'class': 'w-full rounded-lg border border-gray-300 p-2 text-lg focus:outline-none focus:ring-2 focus:ring-blue-500'}),
+            'bio': forms.Textarea(attrs={'class': 'w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500',
+                'rows': 4}),
+
+        }
 
 class LinkForm(forms.ModelForm):
     class Meta:
