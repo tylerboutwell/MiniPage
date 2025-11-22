9afe16c6f6768868fe01dc831efe123417848ca6
Tyler Boutwell <tylerboutwell1@gmail.com>
2025-11-22 11:04:55 -0600

    change database settings to use local sqlite for development settings and postgresql for production

--- minipage/settings.py
+++ minipage/settings.py
@@ -77,16 +77,22 @@ TEMPLATES = [
 WSGI_APPLICATION = 'minipage.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-DATABASES = {
-    'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
-        conn_max_age=600
-    )
-}
+if DEBUG:
+    # Local development: SQLite
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
+        'default': dj_database_url.config(
+            # Replace this value with your local database's connection string.
+            default='postgresql://postgres:postgres@localhost:5432/mysite',
+            conn_max_age=600
+        )
+    }
 
 
 # Password validation
