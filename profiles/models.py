668531884ed73823b21c20547c3b423050bcea1e
Tyler Boutwell <tylerboutwell1@gmail.com>
2025-11-29 10:05:20 -0600

    add theme to profile model

--- profiles/models.py
+++ profiles/models.py
@@ -5,6 +5,20 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     profile_name = models.CharField(max_length=100)
     bio = models.TextField()
+    theme = models.CharField(
+    max_length=50,
+    default="light",
+    choices=[
+        ("light", "Light"),
+        ("dark", "Dark"),
+        ("cupcake", "Cupcake"),
+        ("forest", "Forest"),
+        ("dracula", "Dracula"),
+        ("cyberpunk", "Cyberpunk"),
+        ("synthwave", "Synthwave"),
+        ("retro", "Retro"),
+    ],
+    )
 
 class Link(models.Model):
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='links')
