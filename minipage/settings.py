cf7ef180cfcf6207ba3357220893bc8b6bd79837
Tyler Boutwell <tylerboutwell1@gmail.com>
2025-12-19 19:47:24 -0600

    get aws region from env

--- minipage/settings.py
+++ minipage/settings.py
@@ -153,7 +153,7 @@ else:
     AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
     AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
-    AWS_S3_REGION_NAME = "auto"
+    AWS_S3_REGION_NAME = os.getenv("AWS_REGION")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
